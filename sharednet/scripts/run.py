564f8d9909a10da249d1ac2e3012bc25b25d1c1e
jjia <972931271@qq.com>
2022-04-26 22:02:22 +0200

    Add a evaluator for all tasks. Inference net should be gotten from the same weights. log inference metrics. Add the gradient accumulation for multiple tasks training. join multiple threadings to avoid multiple mlflow runs.

--- sharednet/scripts/run.py
+++ sharednet/scripts/run.py
@@ -26,13 +26,16 @@ from sharednet.modules.path import Mypath, MypathDataDir
 from sharednet.modules.evaluator import get_evaluator
 from sharednet.modules.dataset import DataAll
 from sharednet.modules.evaluator import get_inferer
+import statistics
 
 from monai.metrics import DiceMetric
 import monai
 import os
 import seg_metrics.seg_metrics as sg
-
-
+import shutil
+from medutils.medutils import get_all_ct_names
+import pathlib
+import glob
 from argparse import Namespace
 
 args = get_args()
@@ -81,7 +84,7 @@ def task_of_model(model_name):
 
 
 def all_loaders(model_name):
-    data = DataAll(model_name)
+    data = DataAll(model_name, psz=args.psz)
     # if model_name == 'lobe_ll':
     #     data = DataLobeLL()
     # elif model_name == 'lobe_lu':
@@ -149,21 +152,22 @@ class Task:
         self.opt = opt
         self.loss_fun = loss_fun
         self.device = torch.device("cuda")
-        data, self.tr_dl, self.vd_dl, self.ts_dl = all_loaders(self.model_name)
+        _, self.tr_dl, self.vd_dl, self.ts_dl = all_loaders(self.model_name)
         self.tr_dl_endless = loop_dl(self.tr_dl, args.batch_size)  # loop training dataset
+        self.psz_xy = int(args.psz.split("_")[0])
+        self.psz_z = int(args.psz.split("_")[1])
 
-
-        self.eval_vd = get_evaluator(net, self.vd_dl, self.mypath, data.psz_xy, data.psz_z, args.batch_size, 'valid',
+        self.eval_vd = get_evaluator(net, self.vd_dl, self.mypath, self.psz_xy, self.psz_z, args.batch_size, 'valid',
                                         out_chn)
-        self.eval_ts = get_evaluator(net, self.ts_dl, self.mypath, data.psz_xy, data.psz_z, args.batch_size, 'test',
+        self.eval_ts = get_evaluator(net, self.ts_dl, self.mypath, self.psz_xy, self.psz_z, args.batch_size, 'test',
                                        out_chn)
         self.accumulate_loss = 0
         self.accumulate_dice_ex_bg = 0
 
         self.dice_fun_ex_bg = monai.losses.DiceLoss(to_onehot_y=True, softmax=True, include_background=False)
-        self.inferer = get_inferer(data.psz_xy, data.psz_z, args.batch_size, 'infer')
+        self.inferer = get_inferer(self.psz_xy, self.psz_z, args.batch_size, 'infer')
 
-    def step(self, step_id):
+    def step(self, step_id, accu_grad, accu_grad_nb):
 
         self.scaler = torch.cuda.amp.GradScaler()
 
@@ -174,7 +178,7 @@ class Task:
         image, mask, cond = image.to(self.device), mask.to(self.device), cond.to(self.device)
         t3 = time.time()
 
-        self.opt.zero_grad()
+
         if args.amp:
             print('using amp ', end='')
             with torch.cuda.amp.autocast():
@@ -182,19 +186,39 @@ class Task:
                 loss = self.loss_fun(pred, mask)
                 dice_ex_bg = self.dice_fun_ex_bg(pred, mask)
             t4 = time.time()
-            self.scaler.scale(loss).backward()
-            t_bw = time.time()
-            self.scaler.step(self.opt)
-            t_st = time.time()
-            self.scaler.update()
+            if args.grad_accu:
+                self.scaler.scale(loss/accu_grad_nb).backward()
+                t_bw = time.time()
+                if not accu_grad:
+                    self.scaler.step(self.opt)
+                    t_st = time.time()
+                    self.scaler.update()
+                    self.opt.zero_grad()
+                    print('update grad', end='' )
+            else:
+                self.scaler.scale(loss).backward()
+                t_bw = time.time()
+                self.scaler.step(self.opt)
+                t_st = time.time()
+                self.scaler.update()
+                self.opt.zero_grad()
         else:
             print('do not use amp ', end='')
             pred = self.net(image, cond)
             loss = self.loss_fun(pred, mask)
             dice_ex_bg = self.dice_fun_ex_bg(pred, mask)
             t4 = time.time()
-            loss.backward()
-            self.opt.step()
+            if args.grad_accu:
+                (loss / accu_grad_nb).backward()
+                if not accu_grad:
+                    self.opt.step()
+                    self.opt.zero_grad()
+                    print('update grad', end='' )
+
+            else:
+                loss.backward()
+                self.opt.step()
+                self.opt.zero_grad()
         t5 = time.time()
 
         self.accumulate_loss += loss.item()
@@ -207,38 +231,39 @@ class Task:
 
             self.accumulate_loss = 0
             self.accumulate_dice_ex_bg = 0
+        log_metric('TimeLoadBatch', t2-t1, step_id)
         if args.amp:
             print(f" {self.model_name} loss: {loss:.3f}, "
                   f"load batch cost: {t2-t1:.1f}, "
                   f"forward costs: {t4-t3:.1f}, "
-                  f"only backward costs: {t_bw-t4:.1f}; "
-                  f"only step costs: {t_st-t_bw:.1f}; "
-                  f"only update costs: {t5-t_st:.1f}; ", end='' )
+                  # f"only backward costs: {t_bw-t4:.1f}; "
+                  # f"only step costs: {t_st-t_bw:.1f}; "
+                  f"backward costs: {t5-t4:.1f}; ", end='' )
         else:
             print(f" {self.model_name} loss: {loss:.3f}, "
                   f"load batch cost: {t2 - t1:.1f}, "
                   f"forward costs: {t4 - t3:.1f}, "
                   f"only update costs: {t5 - t4:.1f}; ", end='')
 
-
-    def do_validation_if_need(self, step_id, steps, valid_period):
-
-        if step_id % valid_period == 0 or step_id == steps - 1:
-            print(f"start a valid for {self.model_name}")
-            self.eval_vd.run()
+    # def do_validation(self):
         # if step_id == steps - 1:
         #     print(f"start a test for {self.model_name}")
         #     self.eval_ts.run()
 
     def infer(self):
         print("start infer")
-        keys = ("image",)
+        # if args.mode == 'train':  # inference the current running project
+        model_dir = self.mypath.id_dir  # use the weights from the id_dir
+        infer_weights_fpath = best_model_path(model_dir)  # load the saved best model
+        self.net.load_state_dict(torch.load(infer_weights_fpath, map_location=self.device))
+
+        keys = ("image","cond")
         self.net.eval()
         if args.infer_data_dir == '':  # prediction may be GLUCOLD or LUNA16 or LOLA11
-            prediction_folder = os.path.join(self.mypath.infer_pred_dir())
+            prediction_folder = os.path.join(self.mypath.infer_pred_dir)
         else:
             prediction_folder = os.path.join(self.mypath.infer_pred_dir(), args.infer_data_dir.split("/")[-1])
-        print(prediction_folder)
+        print(f"prediction_folder: {prediction_folder}")
         saver = monai.data.NiftiSaver(output_dir=prediction_folder,
                                       mode="nearest")  # todo: change mode , mode="nearest"
         # if 1:
@@ -246,45 +271,47 @@ class Task:
         with torch.no_grad():
             for infer_data in self.infer_loader:
                 print(f"segmenting {infer_data['image_meta_dict']['filename_or_obj']}")
-                preds = self.inferer(infer_data[keys[0]].to(self.device), self.net)
+                preds = self.inferer((infer_data[keys[0]].to(self.device),
+                                      infer_data[keys[1]].to(self.device)), self.net)
                 preds = (preds.argmax(dim=1, keepdims=True)).float()
                 saver.save_batch(preds, infer_data["image_meta_dict"])
 
         # copy the saved segmentations into the required folder structure for submission
-        # if not os.path.exists(prediction_folder):
-        #     os.makedirs(prediction_folder)
-        # # files = glob.glob(os.path.join(prediction_folder,"*", "*.nii.gz"))
-        # files = get_all_ct_names(os.path.join(prediction_folder, "*"), suffix="_ct_seg")
-        #
-        # for f in files:
-        #     new_name = os.path.basename(f)
-        #     new_name = new_name.split("_ct")[0] + new_name.split("_ct")[1]
-        #     to_name = os.path.join(prediction_folder, new_name)
-        #     shutil.move(f, to_name)
-        #     parent_dir = pathlib.Path(f).parent.absolute()  # remove the empty directory after move its file
-        #     if len(os.listdir(parent_dir)) == 0:
-        #         # removing the file using the os.remove() method
-        #         os.rmdir(parent_dir)
-        #
-        # logging.info(f"predictions copied to {prediction_folder}.")
+        if not os.path.exists(prediction_folder):
+            os.makedirs(prediction_folder)
+        # files = glob.glob(os.path.join(prediction_folder,"*", "*.nii.gz"))
+        files = get_all_ct_names(os.path.join(prediction_folder, "*"), suffix="_ct_seg")
 
-        # if "lobe" in self.net_name:  # other tasks do not have lobe ground truth
-        #     gdth_file = self.mypath.data_task_dir
-        #     pred_file = prediction_folder
-        #     csv_file = os.path.join(prediction_folder, 'metrics.csv')
-        #
-        #     metrics = sg.write_metrics(labels=self.labels[1:],  # exclude background
-        #                                gdth_path=gdth_file,
-        #                                pred_path=pred_file,
-        #                                csv_file=csv_file)
-        #     print('metrics: ', metrics)
+        for f in files:
+            new_name = os.path.basename(f)
+            new_name = new_name.split("_ct")[0] + new_name.split("_ct")[1]
+            to_name = os.path.join(prediction_folder, new_name)
+            shutil.move(f, to_name)
+            parent_dir = pathlib.Path(f).parent.absolute()  # remove the empty directory after move its file
+            if len(os.listdir(parent_dir)) == 0:
+                # removing the file using the os.remove() method
+                os.rmdir(parent_dir)
 
-        sg.write_metrics(labels=[1],  # exclude background
+        print(f"predictions moved to {prediction_folder}.")
+
+        all_metrics_ls = sg.write_metrics(labels=[1],  # exclude background
                          gdth_path=self.data_dir,
                          pred_path=prediction_folder,
                          csv_file=prediction_folder + '/metric.csv',
                          metrics=['dice', 'jaccard', 'precision', 'recall', 'fpr', 'fnr', 'vs', 'hd', 'hd95', 'msd',
                                   'mdsd', 'stdsd'])
+        infer_dice = statistics.mean([metric['dice'][0] for metric in all_metrics_ls])
+        infer_msd = statistics.mean([metric['msd'][0] for metric in all_metrics_ls])
+
+        log_metric(self.model_name + 'InferDice', infer_dice)
+        log_metric(self.model_name + 'InferMSD', infer_msd)
+
+
+def best_model_path(model_dir):
+    model_fpaths = glob.glob(str(model_dir) + "/net_key_metric=*.pt")
+    metrics = [float(file_name.split('/')[-1][15:-3]) for file_name in model_fpaths]
+    idx = metrics.index(max(metrics))
+    return model_fpaths[idx]
 
 
 def task_dt(model_names, net, out_chn, opt, loss_fun):
@@ -302,31 +329,51 @@ def run(args: Namespace):
         args: argparse instance
 
     """
+    model_names: List[str] = mt_netnames(args.model_names)
+
     out_chn = get_out_chn(args.model_names)
     log_param('out_chn', out_chn)
-
     net = get_net(args.cond_flag, args.cond_method, args.cond_pos, out_chn, args.base)
     net_parameters = count_parameters(net)
     net_parameters = str(round(net_parameters / 1024 / 1024, 2))
     log_param('net_parameters_M', net_parameters)
     device = torch.device("cuda")
     net = net.to(device)
-    if args.infer_weights_fpath:  # load trained weights
-        net.load_state_dict(torch.load(args.infer_weights_fpath, map_location=device))
+    # if args.infer_ID:  # load trained weights
+    #     if len(model_names)==1:
+    #         task = model_names[0]
+    #     else:
+    #         task = 'liver'  # use the weights saved for liver
+    #     model_dir = Mypath(args.infer_ID, check_id_dir=False, task= task).id_task_dir
+    #     infer_weights_fpath = best_model_path(model_dir)
+    #     net.load_state_dict(torch.load(infer_weights_fpath, map_location=device))
 
     loss_fun = get_loss(loss=args.loss)
 
     opt = torch.optim.Adam(net.parameters(), lr=args.lr, weight_decay=args.weight_decay)  # weight decay is L2 weight norm
 
-    model_names: List[str] = mt_netnames(args.model_names)
     ta_dict = task_dt(model_names, net, out_chn, opt, loss_fun)
+    ta_ls = list(ta_dict.values())
+    vd_dl_all = [data_batch for ta in ta_ls for data_batch in ta.vd_dl]
+    mypath_all = Mypath(args.id, check_id_dir=False)
+    psz_xy = int(args.psz.split("_")[0])
+    psz_z = int(args.psz.split("_")[1])
+    eval_vd_all = get_evaluator(net, vd_dl_all, mypath_all, psz_xy, psz_z, args.batch_size, 'valid', out_chn)
 
     if args.mode != 'infer':
         for step_id in range(args.steps):
             print(f'\nstep number: {step_id}, ', end='' )
+            model_idx = 0
             for model_name, ta in ta_dict.items():
-                ta.step(step_id)
-                ta.do_validation_if_need(step_id, args.steps, args.valid_period)
+                accu_grad = True if model_idx != len(model_names)-1 else False
+                ta.step(step_id, accu_grad=accu_grad, accu_grad_nb=len(model_names))
+                if step_id % args.valid_period == 0 or step_id == args.steps - 1:
+                    print(f"start a valid for {model_name}")
+                    ta.eval_vd.run()
+                    if model_idx==0 and len(model_names)>=1:
+                        eval_vd_all.run()
+                model_idx += 1
+
     for model_name, ta in ta_dict.items():
         ta.infer()
 
@@ -334,17 +381,25 @@ def run(args: Namespace):
 
 
 def record_artifacts(outfile):
-    t = threading.currentThread()
+    mythread = threading.currentThread()
+    mythread.do_run = True
     if outfile:
-        for i in range(6 * 10):  # 10 minutes
-            time.sleep(10)
-            log_artifact(outfile+'_err.txt')
-            log_artifact(outfile+'_out.txt')
+        t = 0
+        while 1:  # stop signal passed from t
+            if mythread.do_run:
+                log_artifact(outfile + '_err.txt')
+                log_artifact(outfile + '_out.txt')
+                if t <= 600:  # 10 minutes
+                    period = 10
+                    t += period
+                else:
+                    period = 60
+                time.sleep(period)
+            else:
+                print('record_artifacts do_run is True, let stop the process')
+                break
 
-        while not getattr(t, "do_run", False):  # stop signal passed from t
-            time.sleep(60)
-            log_artifact(outfile+'_err.txt')
-            log_artifact(outfile+'_out.txt')
+        print('It is time to stop this process: record_artifacts')
         return None
     else:
         print(f"No output file, no log artifacts")
@@ -366,5 +421,10 @@ if __name__ == "__main__":
         log_params(log_dict)
         args.id = id  # do not need to pass id seperately to the latter function
         run(args)
+
+        p1.do_run = False  # stop the thread
         p2.do_run = False  # stop the thread
+        p1.join()
+        p2.join()
+        # time.sleep(80)  # sleep 80 seconds to wait for the finish of other two threads.
         # record_2nd(log_dict=log_dict, args=args)  # write more parameters & metrics to record file.
